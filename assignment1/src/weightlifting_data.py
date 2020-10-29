241aafb0e75083f9681e6d256115f1209f9c067c
ljbevemyr <lisa@bevemyr.com>
2020-10-29 10:31:08 +0100

    addes additional files to the assignment

---
+++ assignment1/src/weightlifting_data.py
@@ -0,0 +1,241 @@
+data = [dict() for _ in range(60)]
+# Problem consisting of 30 plate(s)
+data[0]["plates"] = {56,25,31,53,87,20,28,9,43,64,91,96,74,82,11,65,97,49,5,23,78,18,94,24,51,27,70,47,16,59}
+data[0]["weight"] = 0
+data[0]["expected"] = True
+# Problem consisting of 0 plate(s)
+data[1]["plates"] = {}
+data[1]["weight"] = 848
+data[1]["expected"] = False
+# Problem consisting of 1 plate(s)
+data[2]["plates"] = {79}
+data[2]["weight"] = 79
+data[2]["expected"] = True
+# Problem consisting of 1 plate(s)
+data[3]["plates"] = {92}
+data[3]["weight"] = 82
+data[3]["expected"] = False
+# Problem consisting of 2 plate(s)
+data[4]["plates"] = {77,69}
+data[4]["weight"] = 146
+data[4]["expected"] = True
+# Problem consisting of 2 plate(s)
+data[5]["plates"] = {19,64}
+data[5]["weight"] = 79
+data[5]["expected"] = False
+# Problem consisting of 3 plate(s)
+data[6]["plates"] = {86,75,85}
+data[6]["weight"] = 160
+data[6]["expected"] = True
+# Problem consisting of 3 plate(s)
+data[7]["plates"] = {52,60,69}
+data[7]["weight"] = 103
+data[7]["expected"] = False
+# Problem consisting of 4 plate(s)
+data[8]["plates"] = {43,58,41,6}
+data[8]["weight"] = 101
+data[8]["expected"] = True
+# Problem consisting of 4 plate(s)
+data[9]["plates"] = {79,75,88,18}
+data[9]["weight"] = 98
+data[9]["expected"] = False
+# Problem consisting of 5 plate(s)
+data[10]["plates"] = {7,40,24,9,95}
+data[10]["weight"] = 102
+data[10]["expected"] = True
+# Problem consisting of 5 plate(s)
+data[11]["plates"] = {23,74,72,9,10}
+data[11]["weight"] = 176
+data[11]["expected"] = False
+# Problem consisting of 6 plate(s)
+data[12]["plates"] = {57,48,31,99,44,88}
+data[12]["weight"] = 123
+data[12]["expected"] = True
+# Problem consisting of 6 plate(s)
+data[13]["plates"] = {54,79,38,5,4,80}
+data[13]["weight"] = 34
+data[13]["expected"] = False
+# Problem consisting of 7 plate(s)
+data[14]["plates"] = {86,24,94,3,26,70,12}
+data[14]["weight"] = 120
+data[14]["expected"] = True
+# Problem consisting of 7 plate(s)
+data[15]["plates"] = {11,7,91,10,56,16,53}
+data[15]["weight"] = 62
+data[15]["expected"] = False
+# Problem consisting of 8 plate(s)
+data[16]["plates"] = {70,3,27,13,69,72,4,18}
+data[16]["weight"] = 45
+data[16]["expected"] = True
+# Problem consisting of 8 plate(s)
+data[17]["plates"] = {78,33,69,97,94,80,42,63}
+data[17]["weight"] = 114
+data[17]["expected"] = False
+# Problem consisting of 9 plate(s)
+data[18]["plates"] = {31,45,70,89,55,37,66,76,38}
+data[18]["weight"] = 393
+data[18]["expected"] = True
+# Problem consisting of 9 plate(s)
+data[19]["plates"] = {50,37,39,26,51,31,76,88,7}
+data[19]["weight"] = 100
+data[19]["expected"] = False
+# Problem consisting of 10 plate(s)
+data[20]["plates"] = {49,42,88,59,64,80,97,68,87,76}
+data[20]["weight"] = 363
+data[20]["expected"] = True
+# Problem consisting of 10 plate(s)
+data[21]["plates"] = {59,80,34,96,33,51,37,16,58,32}
+data[21]["weight"] = 89
+data[21]["expected"] = False
+# Problem consisting of 11 plate(s)
+data[22]["plates"] = {77,54,66,52,71,33,41,47,26,24,89}
+data[22]["weight"] = 345
+data[22]["expected"] = True
+# Problem consisting of 11 plate(s)
+data[23]["plates"] = {53,93,85,1,74,96,61,3,91,65,33}
+data[23]["weight"] = 83
+data[23]["expected"] = False
+# Problem consisting of 12 plate(s)
+data[24]["plates"] = {42,24,94,99,58,97,82,91,90,38,39,2}
+data[24]["weight"] = 343
+data[24]["expected"] = True
+# Problem consisting of 12 plate(s)
+data[25]["plates"] = {51,33,84,2,87,48,12,61,46,75,91,86}
+data[25]["weight"] = 572
+data[25]["expected"] = False
+# Problem consisting of 13 plate(s)
+data[26]["plates"] = {28,73,38,52,51,66,33,93,62,11,61,18,69}
+data[26]["weight"] = 380
+data[26]["expected"] = True
+# Problem consisting of 13 plate(s)
+data[27]["plates"] = {53,99,92,28,88,68,40,46,55,84,52,76,15}
+data[27]["weight"] = 115
+data[27]["expected"] = False
+# Problem consisting of 14 plate(s)
+data[28]["plates"] = {76,25,93,49,53,78,89,95,88,94,35,40,42,98}
+data[28]["weight"] = 166
+data[28]["expected"] = True
+# Problem consisting of 14 plate(s)
+data[29]["plates"] = {62,60,81,16,82,63,40,83,18,38,2,76,90,78}
+data[29]["weight"] = 107
+data[29]["expected"] = False
+# Problem consisting of 15 plate(s)
+data[30]["plates"] = {63,14,99,10,12,71,52,75,5,29,16,80,17,85,77}
+data[30]["weight"] = 259
+data[30]["expected"] = True
+# Problem consisting of 15 plate(s)
+data[31]["plates"] = {48,73,3,22,18,65,70,13,99,76,100,16,68,75,59}
+data[31]["weight"] = 8
+data[31]["expected"] = False
+# Problem consisting of 16 plate(s)
+data[32]["plates"] = {27,82,51,23,14,91,13,88,12,73,81,15,78,37,61,29}
+data[32]["weight"] = 196
+data[32]["expected"] = True
+# Problem consisting of 16 plate(s)
+data[33]["plates"] = {99,92,95,6,47,74,41,7,83,88,9,86,4,45,91,55}
+data[33]["weight"] = 76
+data[33]["expected"] = False
+# Problem consisting of 17 plate(s)
+data[34]["plates"] = {24,94,20,90,60,38,89,80,87,46,32,76,72,75,16,67,7}
+data[34]["weight"] = 289
+data[34]["expected"] = True
+# Problem consisting of 17 plate(s)
+data[35]["plates"] = {92,56,49,58,67,43,75,17,48,15,1,29,39,98,97,64,82}
+data[35]["weight"] = 42
+data[35]["expected"] = False
+# Problem consisting of 18 plate(s)
+data[36]["plates"] = {77,55,25,9,4,1,10,83,26,57,79,21,27,24,60,36,46,73}
+data[36]["weight"] = 527
+data[36]["expected"] = True
+# Problem consisting of 18 plate(s)
+data[37]["plates"] = {70,83,91,85,23,17,93,26,2,13,57,56,66,94,55,29,65,31}
+data[37]["weight"] = 20
+data[37]["expected"] = False
+# Problem consisting of 19 plate(s)
+data[38]["plates"] = {75,33,28,99,6,93,94,47,34,20,35,78,46,81,67,66,56,91,96}
+data[38]["weight"] = 877
+data[38]["expected"] = True
+# Problem consisting of 19 plate(s)
+data[39]["plates"] = {74,65,93,33,19,42,18,51,47,70,84,66,55,40,96,59,94,77,41}
+data[39]["weight"] = 63
+data[39]["expected"] = False
+# Problem consisting of 20 plate(s)
+data[40]["plates"] = {1,9,30,91,23,19,75,74,97,40,34,69,5,16,22,60,94,80,70,86}
+data[40]["weight"] = 421
+data[40]["expected"] = True
+# Problem consisting of 20 plate(s)
+data[41]["plates"] = {69,39,81,84,90,78,68,62,61,54,47,45,42,31,20,1,92,30,11,35}
+data[41]["weight"] = 29
+data[41]["expected"] = False
+# Problem consisting of 21 plate(s)
+data[42]["plates"] = {82,93,58,2,96,86,71,39,33,30,52,5,83,17,38,40,32,14,4,87,64}
+data[42]["weight"] = 91
+data[42]["expected"] = True
+# Problem consisting of 21 plate(s)
+data[43]["plates"] = {59,6,22,48,62,52,93,25,18,3,75,91,53,68,82,32,85,10,38,40,28}
+data[43]["weight"] = 988
+data[43]["expected"] = False
+# Problem consisting of 22 plate(s)
+data[44]["plates"] = {49,44,69,31,66,1,43,4,83,20,84,52,67,14,24,28,17,11,47,19,46,12}
+data[44]["weight"] = 566
+data[44]["expected"] = True
+# Problem consisting of 22 plate(s)
+data[45]["plates"] = {58,23,1,46,91,29,55,8,51,82,76,66,21,20,94,39,81,89,42,79,15,6}
+data[45]["weight"] = 17
+data[45]["expected"] = False
+# Problem consisting of 23 plate(s)
+data[46]["plates"] = {82,15,54,34,31,94,85,72,91,44,81,60,5,73,88,4,98,39,9,2,41,59,50}
+data[46]["weight"] = 1056
+data[46]["expected"] = True
+# Problem consisting of 23 plate(s)
+data[47]["plates"] = {42,34,6,81,11,21,96,24,99,49,90,17,100,67,47,82,85,88,16,87,79,73,9}
+data[47]["weight"] = 5
+data[47]["expected"] = False
+# Problem consisting of 24 plate(s)
+data[48]["plates"] = {2,43,70,11,4,33,53,60,14,59,16,51,77,31,25,69,93,90,29,12,91,15,1,17}
+data[48]["weight"] = 750
+data[48]["expected"] = True
+# Problem consisting of 24 plate(s)
+data[49]["plates"] = {20,27,57,61,32,78,55,13,58,92,81,51,49,91,75,44,43,88,19,42,96,40,3,98}
+data[49]["weight"] = 41
+data[49]["expected"] = False
+# Problem consisting of 25 plate(s)
+data[50]["plates"] = {67,25,9,41,65,15,66,73,36,70,2,22,96,82,34,30,100,40,12,45,61,14,32,94,13}
+data[50]["weight"] = 596
+data[50]["expected"] = True
+# Problem consisting of 25 plate(s)
+data[51]["plates"] = {94,76,72,68,74,51,1,89,96,55,85,40,20,52,22,100,67,30,44,13,64,9,50,3,75}
+data[51]["weight"] = 19
+data[51]["expected"] = False
+# Problem consisting of 26 plate(s)
+data[52]["plates"] = {38,27,24,65,86,26,13,11,93,2,4,50,75,88,58,82,67,92,9,60,39,6,1,99,63,22}
+data[52]["weight"] = 757
+data[52]["expected"] = True
+# Problem consisting of 26 plate(s)
+data[53]["plates"] = {82,40,3,16,55,10,35,56,67,72,36,54,1,94,81,98,19,8,44,97,53,86,49,51,25,52}
+data[53]["weight"] = 15
+data[53]["expected"] = False
+# Problem consisting of 27 plate(s)
+data[54]["plates"] = {42,78,45,79,9,64,94,72,10,67,5,6,73,48,69,90,86,13,95,51,61,18,3,55,85,50,76}
+data[54]["weight"] = 1123
+data[54]["expected"] = True
+# Problem consisting of 27 plate(s)
+data[55]["plates"] = {12,92,89,26,15,93,65,4,35,16,98,66,22,32,19,48,55,62,94,5,18,100,81,41,59,91,2}
+data[55]["weight"] = 10
+data[55]["expected"] = False
+# Problem consisting of 28 plate(s)
+data[56]["plates"] = {35,13,24,81,9,68,57,80,33,19,27,74,58,6,36,59,48,38,32,28,30,86,44,63,25,79,72,64}
+data[56]["weight"] = 164
+data[56]["expected"] = True
+# Problem consisting of 28 plate(s)
+data[57]["plates"] = {51,23,44,45,68,3,47,74,100,43,10,76,16,97,21,99,87,28,84,32,83,41,38,71,15,11,73,42}
+data[57]["weight"] = 4
+data[57]["expected"] = False
+# Problem consisting of 29 plate(s)
+data[58]["plates"] = {63,19,57,26,7,96,4,100,16,72,34,39,87,62,49,54,97,94,67,32,85,58,46,31,82,74,83,14,17}
+data[58]["weight"] = 1428
+data[58]["expected"] = True
+# Problem consisting of 29 plate(s)
+data[59]["plates"] = {95,48,33,9,23,54,14,18,74,35,97,50,44,7,45,64,27,52,58,85,83,92,76,53,77,51,40,8,4}
+data[59]["weight"] = 1
+data[59]["expected"] = False
