b5e786dcc2d43adbbdfac1f3c3bd94e5013bdff1
ljbevemyr <lisa@bevemyr.com>
2020-11-27 09:06:43 +0100

    comments

--- assignment2/recompute_mst.py
+++ assignment2/recompute_mst.py
@@ -3,8 +3,8 @@
 '''
 Assignment 2, Problem 2: Recomputing a Minimum Spanning Tree
 
-Team Number:
-Student Names:
+Team Number: 11
+Student Names: Lisa Bevemyr and Maja Danielsson
 '''
 
 '''
@@ -21,7 +21,7 @@ appear publicly on the internet, both during a course instance and
 forever after.
 '''
 from src.recompute_mst_data import data  # noqa
-from typing import Tuple  # noqa
+from typing import Tuple, Set  # noqa
 from src.graph import Graph  # noqa
 from math import inf
 import unittest  # noqa
@@ -41,46 +41,23 @@ import logging  # noqa
 
 __all__ = ['update_MST_1', 'update_MST_2', 'update_MST_3', 'update_MST_4']
 
-'''
-def update_MST_1(G: Graph, T: Graph, e: Tuple[str, str], weight: int):
+def dfs(T: Graph, node: str, nodes: Set[str]):
     """
-    Sig:  Graph G(V, E), Graph T(V, E), edge e, int ->
-    Pre:
-    Post:
-    Ex:   TestCase 1 below
+    Sig:  T: Graph, node: str, nodes: Set[str] ->
+    Pre:  node must exist in T and nodes must be empty.
+    Post: nodes contains all visited nodes
+    Ex:   T = <V=(a,b,c,d,e), E=((a, b),(b, c),(c, d))>
+          node = 'a'
+          nodes = {}
+          dfs(T, node, nodes)
+          nodes is now {a,b,c,d}
     """
-    (u, v) = e
-    assert(e in G and e not in T and weight > G.weight(u, v))
-
-
-def update_MST_2(G: Graph, T: Graph, e: Tuple[str, str], weight: int):
-    """
-    Sig:  Graph G(V, E), Graph T(V, E), edge e, int ->
-    Pre:
-    Post:
-    Ex:   TestCase 2 below
-    """
-    (u, v) = e
-    assert(e in G and e not in T and weight < G.weight(u, v))
-
-
-def update_MST_3(G: Graph, T: Graph, e: Tuple[str, str], weight: int):
-    """
-    Sig:  Graph G(V, E), Graph T(V, E), edge e, int ->
-    Pre:
-    Post:
-    Ex:   TestCase 3 below
-    """
-    (u, v) = e
-    assert(e in G and e in T and weight < G.weight(u, v))
-'''
-def dfs(T, node, nodes):
     nodes.add(node)
     for neighbour in T.neighbors(node):
-        # Invariant: visited contains one new neighbour and all previous neighbours
-        # Variant: len(graph.neighbors(node)) - graph.neighbors.index(neighbour)
+        # Variant: len(T.neighbors(node)) - T.neighbors.index(neighbour)
         if neighbour not in nodes:
             dfs(T, neighbour, nodes)
+            # Variant: len(T.nodes) - len(nodes)
 
 def update_MST_4(G: Graph, T: Graph, e: Tuple[str, str], weight: int):
     """
@@ -102,127 +79,16 @@ def update_MST_4(G: Graph, T: Graph, e: Tuple[str, str], weight: int):
 
     min_edge = e
     for (l, m) in all_edges:
+        # Variant: len(all_edges) - all_edges.index((l, m))
         if (l in a_nodes and m not in a_nodes) or (m in a_nodes and l not in a_nodes):
             (u, v) = min_edge
             if G.weight(l, m) < G.weight(u, v):
                 min_edge = (l, m)
-    
+
     (u, v) = min_edge
     T.add_edge(u, v, G.weight(u, v))
 
 
 
 
-class RecomputeMstTest(unittest.TestCase):
-    """
-    Test Suite for minimum spanning tree problem
 
-    Any method named "test_something" will be run when this file is
-    executed. You may add your own test cases if you wish.
-    (You may delete this class from your submitted solution.)
-    """
-    logger = logging.getLogger('RecomputeMstTest')
-
-    def assertUndirectedEdgesEqual(self, actual, expected):
-        self.assertListEqual(
-            sorted(((min(u, v), max(u, v)) for u, v in actual)),
-            sorted(((min(u, v), max(u, v)) for u, v in expected))
-        )
-
-    def assertGraphIsConnected(self, graph):
-        if len(graph.nodes) == 0:
-            return
-        visited = set()
-        s = graph.nodes[0]
-        queue = deque([s])
-        while len(queue) > 0:
-            u = queue.popleft()
-            visited.add(u)
-            for v in graph.neighbors(u):
-                if v not in visited:
-                    queue.append(v)
-        for u in graph.nodes:
-            self.assertIn(u, visited)
-
-    def assertEdgesInGraph(self, edges, graph):
-        for edge in edges:
-            self.assertIn(edge, graph)
-    '''
-    def test_mst1(self):
-        # TestCase 1: e in graph.edges and e not in tree.edges and
-        #             weight > graph.weight(u, v)
-        i = 0
-        for instance in data:
-            graph = instance['graph'].copy()
-            tree = instance['mst'].copy()
-            u, v = instance['solutions'][i]['edge']
-            weight = instance['solutions'][i]['weight']
-            expected = instance['solutions'][i]['expected']
-            update_MST_1(graph, tree, (u, v), weight)
-            self.assertUndirectedEdgesEqual(
-                tree.edges,
-                expected
-            )
-
-    def test_mst2(self):
-        # TestCase 2: e in graph.edges and not e in tree.edges and
-        #             weight < graph.weight(u, v)
-        i = 1
-        for instance in data:
-            graph = instance['graph'].copy()
-            tree = instance['mst'].copy()
-            u, v = instance['solutions'][i]['edge']
-            weight = instance['solutions'][i]['weight']
-            expected = instance['solutions'][i]['expected']
-            update_MST_2(graph, tree, (u, v), weight)
-            self.assertUndirectedEdgesEqual(
-                tree.edges,
-                expected
-            )
-
-    def test_mst3(self):
-        # TestCase 3: e in graph.edges and e in tree and
-        #             weight < graph.weight(u, v)
-        i = 2
-        for instance in data:
-            graph = instance['graph'].copy()
-            tree = instance['mst'].copy()
-            u, v = instance['solutions'][i]['edge']
-            weight = instance['solutions'][i]['weight']
-            expected = instance['solutions'][i]['expected']
-            update_MST_3(graph, tree, (u, v), weight)
-            self.assertUndirectedEdgesEqual(
-                tree.edges,
-                expected
-            )
-    '''
-    def test_mst4(self):
-        # TestCase 4: e in graph.edges and e in tree and
-        #             weight > graph.weight(u, v)
-        i = 3
-        for instance in data:
-            graph = instance['graph'].copy()
-            tree = instance['mst'].copy()
-            u, v = instance['solutions'][i]['edge']
-            weight = instance['solutions'][i]['weight']
-            expected = instance['solutions'][i]['expected']
-            expected_G = graph.copy()
-            expected_G.set_weight(u, v, weight)
-            update_MST_4(graph, tree, (u, v), weight)
-            self.assertEdgesInGraph(tree.edges, expected_G)
-            self.assertGraphIsConnected(tree)
-            self.assertEqual(
-                sum(graph.weight(u, v) for u, v in tree.edges),
-                sum(graph.weight(u, v) for u, v in expected)
-            )
-            for u, v in tree.edges:
-                self.assertEqual(
-                    tree.weight(u, v),
-                    expected_G.weight(u, v)
-                )
-
-
-if __name__ == '__main__':
-    # Set logging config to show debug messages.
-    logging.basicConfig(level=logging.DEBUG)
-    unittest.main()
